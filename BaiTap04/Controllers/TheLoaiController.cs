0d56fc7c775fa24f5a6ad427017a48dc85f28507
Sangpham26 <sangpham0231@gmail.com>
2024-09-06 13:47:55 +0700

    BaiTap04

---
+++ BaiTap04/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
