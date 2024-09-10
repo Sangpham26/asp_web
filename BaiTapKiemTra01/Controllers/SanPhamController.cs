4bd0c99f47e358ca0b13a5bae0ab3b45588ac75d
Sangpham26 <sangpham0231@gmail.com>
2024-09-10 14:29:47 +0700

    Bai Kiem Tra

---
+++ BaiTapKiemTra01/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BaiTap02()
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSanPham = "VGA ASUS ROG Strix GeForce RTX 4090",
+                GiaBan = 6000, 
+                AnhMoTa = "/images/4090.jpg"
+            };
+
+            return View(sanpham);
+        }
+    }
+}
---
+++ BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,21 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (model.TenTaiKhoan != null)
+            {
+                return Content(model.TenTaiKhoan);
+            }
+            return View(model);
+        }
+    }
+}
