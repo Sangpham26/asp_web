78c3a96f54fdd08364a2fd046d6f38f017633de6
Sangpham26 <sangpham0231@gmail.com>
2024-09-17 08:18:24 +0700

    BaiTap06

---
+++ BaiTap06/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+
+namespace BaiTap06.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int ID { get; set; }
+        public string Username {  get; set; }
+        public string Password { get; set; }
+    }
+}
